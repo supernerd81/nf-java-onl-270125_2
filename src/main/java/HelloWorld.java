8521bce120da97e4b16d59d70d4c57144b5d8131
Manuel Simon <info@der-supernerd.de>
2025-01-28 14:34:55 +0100

    add HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int a = 5580;
+        int b = 6561;
+        int sum = a + b;
+
+        float c = 333;
+        float d = 3;
+
+        double j = 232;
+        double f = 23;
+
+        String ausgabe = "Java ist cool!";
+        System.out.println(a * b);
+        System.out.println(a < b);
+        System.out.println(c + d);
+        System.out.println(c < d);
+        System.out.println(j - f);
+        System.out.println(j < f);
+    }
+}
